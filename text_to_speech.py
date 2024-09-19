2e3c8f185c184153db6a5d8153a110874903b03b
Gabriel Patara <gabriel.patara@gmail.com>
2024-09-19 13:20:21 -0300

    v0.0.2 - text_to_speech offline

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,19 +1,36 @@
-from gtts import gTTS
-from playsound import playsound
+import pyttsx3
+import os
 
-# Ler o conteúdo de um arquivo de texto
-file_path = 'fala.txt'
+# Caminho para o arquivo de saída
+audio_file = 'output.wav'
 
-# Abrir o arquivo em modo de leitura
-with open(file_path, 'r', encoding='utf-8') as file:
-    content = file.read()  # Lê o conteúdo do arquivo
+# Inicializar o mecanismo TTS
+engine = pyttsx3.init()
 
+# Configurar a taxa de fala
+engine.setProperty('rate', 200)  # Ajuste a velocidade conforme necessário
 
-# Configurar a linguagem e gerar o arquivo de áudio
-tts = gTTS(text=content, lang='pt-br')
+# Configurar o volume
+engine.setProperty('volume', 1)  # Volume entre 0 e 1
 
-# Salvar o arquivo de áudio
-audio_file = "output.mp3"
-tts.save("output.mp3")
+# Texto para converter em áudio
+text = "Olá, este é um exemplo de texto convertido em áudio."
 
-playsound(audio_file)
+# Salvar o áudio em um arquivo WAV
+engine.save_to_file(text, audio_file)
+
+# Executar o áudio
+engine.runAndWait()
+
+# Reproduzir o áudio
+try:
+    import winsound
+    winsound.PlaySound(audio_file, winsound.SND_FILENAME)
+except ImportError:
+    print(f"Por favor, instale o módulo 'winsound' para reprodução de áudio no Windows.")
+except Exception as e:
+    print(f"Ocorreu um erro ao tentar reproduzir o áudio: {e}")
+
+# Limpar o arquivo de áudio depois de reproduzir
+if os.path.exists(audio_file):
+    os.remove(audio_file)
